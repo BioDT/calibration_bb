59bbb93bd345dcb7ccac35294323d32e1db9d768
Tuomas Rossi <tuomas.rossi@csc.fi>
2024-08-12 07:41:13 +0300

    Simplify optimization

--- run_optimization_Ackley.py
+++ run_optimization_Ackley.py
@@ -1,18 +1,22 @@
-from pymoo.algorithms.moo.nsga2 import NSGA2
-from pymoo.termination import get_termination
-from pymoo.core.problem import Problem
 import numpy as np
-import sys
-from numpy import inf
+import matplotlib as mpl
+from matplotlib import pyplot as plt
 
-import shutil
-import pandas as pd
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.core.problem import Problem
+from pymoo.optimize import minimize
+from pymoo.visualization.scatter import Scatter
+from pymoo.visualization.heatmap import Heatmap
 
-from pathlib import Path
 
+# Fix matplolib compatibility issue in pymoo
+mpl.cm.get_cmap = plt.get_cmap
 
-# Define the Ackley function for external evaluation
+# Define the Ackley function
 def ackley_function(X):
+    if X.ndim == 1:
+        X = X[np.newaxis]
+
     a = 20
     b = 0.2
     c = 2 * np.pi
@@ -20,75 +24,62 @@ def ackley_function(X):
 
     sum1 = np.sum(X**2, axis=1)
     sum2 = np.sum(np.cos(c * X), axis=1)
-    
+
     term1 = -a * np.exp(-b * np.sqrt(sum1 / d))
     term2 = -np.exp(sum2 / d)
-    
+
     F = term1 + term2 + a + np.exp(1)
-    
-    # Ensure F is a 2D array
-    return F[:, np.newaxis]
+    return F
 
 
-# Define a custom problem class that pymoo will use
+# Define a problem class that pymoo will use
 class MyAckleyProblem(Problem):
-    def __init__(self, n_var=2):
-        super().__init__(n_var=n_var, n_obj=1, n_constr=0, xl=-32.768, xu=32.768)
+    def __init__(self):
+        super().__init__(n_var=2, n_obj=1, n_constr=0, xl=-32.768, xu=32.768)
 
-    def _evaluate(self, X, out, *args, **kwargs):
-        # Evaluation is done externally, so this can be left as pass or a placeholder
-        pass
-
-algorithm = NSGA2(pop_size=300)
-
-termination = get_termination("n_gen", 1000)
+    def _evaluate(self, x, out, *args, **kwargs):
+        out["F"] = ackley_function(x)
 
 
 def main():
-
     problem = MyAckleyProblem()
+    algorithm = NSGA2(pop_size=300)
 
-    # create an algorithm object that never terminates
-    algorithm.setup(problem, termination=termination)
-  
-    # Variables to keep track of the best solution found
-    global_best_X = None
-    global_best_F = np.inf
+    res = minimize(problem,
+                   algorithm,
+                   ('n_gen', 50),
+                   seed=1,
+                   verbose=False)
 
-    for n_gen in range(50):
-    
-        # Step 1: Generate a new population
-        pop = algorithm.ask()
+    print("Optimal point:", res.X)
+    print("value:        ", res.F)
+    print("value check:  ", ackley_function(res.X))
 
-        F = ackley_function(pop.get("X"))
-  
-        # Step 3: Set the evaluated results back to the population
-        pop.set("F", F)
+    # Evaluate on grid for plotting
+    xl, xu = -5, 5
+    yl, yu = -5, 5
+    x, y = np.meshgrid(np.linspace(xl, xu, 100), np.linspace(yl, yu, 100))
+    X = np.asarray([np.ravel(x), np.ravel(y)]).T
+    F = ackley_function(X).reshape(x.shape)
+    print(X.shape, F.shape)
 
-        # Step 4: Provide the evaluated results back to the algorithm
-        algorithm.tell(infills=pop)
+    # Plot
+    plt.figure()
+    plt.pcolormesh(x, y, F)
+    plt.scatter(*res.X)
+    plt.colorbar()
+    plt.savefig('test.png')
 
-        # Update the global best solution
-        best_idx = np.argmin(F)
-        if F[best_idx] < global_best_F:
-            global_best_F = F[best_idx]
-            global_best_X = pop.get("X")[best_idx]
+    # Pymoo functions
+    # plot = Heatmap()
+    # plot.add(F)
+    # plot.save("heatmap.png")
 
-        # Print the ongoing best values found so far
-        print(f"Generation {algorithm.n_gen-1}: Best F = {global_best_F}, Best X = {global_best_X}")
-        print("")
+    # plot = Scatter()
+    # plot.add(problem.pareto_front(), plot_type="line", color="black", alpha=0.7)
+    # plot.add(res.F, facecolor="none", edgecolor="red")
+    # plot.save("scatter.png")
 
-
-    # Get the final results
-    res = algorithm.result()
-
-    # The final population and their evaluated objectives
-#    print("Resulted values:", res.X)
-
-########################################################
 
 if __name__ == '__main__':
     main()
-
-
-
